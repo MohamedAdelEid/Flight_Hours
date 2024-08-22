9e0c0007728ef1f44678dc2f4f1d8a5b41f3fee4
Mohamed Saad <sa3doni2714@gmail.com>
2024-08-22 04:22:52 +0300

    Changes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,12 +20,5 @@ class UserSeeder extends Seeder
             'password' => Hash::make('Admin@123'), // It's a good practice to hash passwords
             'role' => 'admin'
         ]);
-        // User::create([
-        //     'name' => 'Ahlam Taboli',
-        //     'email' => 'ahlam@gmail.com',
-        //     'password' => Hash::make('12345678'), // It's a good practice to hash passwords
-        //     'role' => 'captain'
-        // ]);
-
-    }   
+    }
 }
